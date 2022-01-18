ecfd27deabd48239cccd139b5bdacec30b451866
JimKalp <dimitrisd2538@gmail.com>
2022-01-18 21:52:24 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/button/button.component';
+import { LandmarksComponent } from './components/landmarks/landmarks.component';
+import { LandmarkItemComponent } from './components/landmark-item/landmark-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent,
+    LandmarksComponent,
+    LandmarkItemComponent
   ],
   imports: [
     BrowserModule
