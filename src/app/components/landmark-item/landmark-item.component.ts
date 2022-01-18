c158b40219c5af4cf5a3c14cb6872316589777a0
JimKalp <dimitrisd2538@gmail.com>
2022-01-18 21:38:15 +0200

    Initial commit

---
+++ src/app/components/landmark-item/landmark-item.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Landmark } from 'src/app/Landmark';
+
+@Component({
+  selector: 'app-landmark-item',
+  templateUrl: './landmark-item.component.html',
+  styleUrls: ['./landmark-item.component.css']
+})
+export class LandmarkItemComponent implements OnInit {
+  @Input() landmark: Landmark
+  constructor() { 
+    
+  }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/landmarks/landmarks.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Landmark } from 'src/app/Landmark';
+
+@Component({
+  selector: 'app-landmarks',
+  templateUrl: './landmarks.component.html',
+  styleUrls: ['./landmarks.component.css']
+})
+export class LandmarksComponent implements OnInit {
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
