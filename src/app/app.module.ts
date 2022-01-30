56e2b4d79d2b76879f774a61e212ee70bc85f861
JimKalp <dimitrisd2538@gmail.com>
2022-01-30 21:17:35 +0200

    Changed font family

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
-import { NgImageFullscreenViewModule } from 'ng-image-fullscreen-view';
-import { NgImageSliderModule } from 'ng-image-slider';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -15,7 +13,6 @@ import { LandmarkFullpageComponent } from './components/landmark-fullpage/landma
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { MapComponent } from './components/map/map.component';
-import * as GoogleMapsLoader from 'google-maps';
 
 
 @NgModule({
@@ -34,9 +31,7 @@ import * as GoogleMapsLoader from 'google-maps';
     HttpClientModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule,
-    NgImageFullscreenViewModule,
-    NgImageSliderModule
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
